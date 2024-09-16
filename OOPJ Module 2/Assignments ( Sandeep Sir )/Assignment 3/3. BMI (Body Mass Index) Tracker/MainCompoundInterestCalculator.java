95f88f3e864fe67931f52a01be1b4f4d31af1ead
sahil-pardhi <sahilpardhi1820@gmail.com>
2024-09-16 11:16:36 -0700

    Compound

---
+++ OOPJ Module 2/Assignments ( Sandeep Sir )/Assignment 3/3. BMI (Body Mass Index) Tracker/MainCompoundInterestCalculator.java
@@ -0,0 +1,13 @@
+package methods.fields.example;
+
+public class MainCompoundInterestCalculator {
+
+	public static void main(String[] args) {
+
+		CompoundInterestCalculator c = new CompoundInterestCalculator();
+		c.acceptRecord();
+		c.printRecord();
+		
+	}
+
+}
